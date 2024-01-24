31ec9120640836430a141bb0ccbaa3256678d3c9
Mulyana <mulcoding@gmail.com>
2024-01-24 14:42:29 +0700

    answer exercise 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (words, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += words;
+  }
+  return string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  const split = str.split("");
+  const reverse = split.reverse("");
+  const join = reverse.join("");
 
+  return join;
 };
 
 // Do not edit below this line
